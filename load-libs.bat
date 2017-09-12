a9e71eacde189dd284a5143a80aac10560486033
Tumble <tumblegamer@gmail.com>
2017-09-12 17:02:25 +0100

    Added TBP File structure staded

---
+++ load-libs.bat
@@ -0,0 +1,13 @@
+@if not "%1"=="debug" @echo off
+REM CHECK IF IN A TBP PROJECT
+if not exist .tbp (
+  goto :eof
+)
+pause
+
+REM LOAD PROJECT LIBRARIES
+if "%~dp0"=="%cd:"=%" (
+  for /d %%L in ("lib\*") do (
+    set path=%cd%\%%L
+  )
+)
