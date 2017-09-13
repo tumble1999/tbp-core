7c78dbd646e2f7538b8b996a99fdae20ac83cfc6
Tumble <tumblegamer@gmail.com>
2017-09-13 11:51:39 +0100

    fixed unexected problem

--- update.bat
+++ update.bat
@@ -4,10 +4,11 @@ set actual-remote="https://github.com/tumble1999/tbp-core"
 git remote get-url --push origin>remote-tbp-url.txt
 set /p remote-url=<remote-tbp-url.txt
 del remote-tbp-url.txt
-if not "%remote-url%:"=%=="%actual-remote:"=%" (
+if not "%remote-url%:"=%"=="%actual-remote:"=%" (
   if not exist .git (
     git init
   )
   git remote add origin "%actual-remote:"=%"
 )
 git pull
+pause
