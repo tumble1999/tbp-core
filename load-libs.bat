f9b84686c8bf2e179929520aaa954dac27055807
Tumble <tumblegamer@gmail.com>
2017-09-13 12:10:09 +0100

    fixed load libs

--- load-libs.bat
+++ load-libs.bat
@@ -4,23 +4,20 @@ if not exist .tbp (
   goto :eof
 )
 
-REM LOAD PROJECT LIBRARIES
+rem LOAD PROJECT LIBRARIES
 for /d %%L in ("lib\*") do (
-  set path=%cd%\%%L;%path%
-  set %cd%=path
-  cd %%L
-  start /wait load-libs.bat
-  cd../..
+  set lib=%%L
+  set path=!cd!\!lib!;!path!
+  cd !lib!
+  call load-libs.bat
+  cd ../..
 )
 
 for /d %%L in ("lib\*.tbpl") do (
-  project-tools\compile %%L D L
-  set file=%%L
-  set path=%cd%\%file:0,-5%;%path%
-  set %cd%=path
-  cd %%L
-  start /wait load-libs.bat
-  cd../..
+  set lib=%%L
+  project-tools\compile !lib! D L
+  set path=!cd!\!lib:0,-5!;!path!
+  cd !lib!
+  call load-libs.bat
+  cd ../..
 )
-
-exit
