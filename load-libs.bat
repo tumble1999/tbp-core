d4fe4f5d20883f7978d14ce6c2904e7ba0220c87
Tumble <tumblegamer@gmail.com>
2017-09-13 11:12:41 +0100

    load libs stored in core libraries now

--- load-libs.bat
+++ load-libs.bat
@@ -1,4 +1,4 @@
-@if not "%1"=="debug" @echo off
+@echo off
 REM CHECK IF IN A TBP PROJECT
 if not exist .tbp (
   goto :eof
@@ -6,12 +6,19 @@ if not exist .tbp (
 pause
 
 REM LOAD PROJECT LIBRARIES
-if "%~dp0"=="%cd:"=%" (
-  for /d %%L in ("lib\*") do (
-    set path=%cd%\%%L
-    set %cd%=path
-    cd %%L
-    start /wait load-libs.bat
-    cd../..
-  )
+for /d %%L in ("lib\*") do (
+  set path=%cd%\%%L
+  set %cd%=path
+  cd %%L
+  start /wait load-libs.bat
+  cd../..
+)
+
+for /d %%L in ("lib\*.tbpl") do (
+  project-tools\compile %%L D L
+  set path=%cd%\%%L
+  set %cd%=path
+  cd %%L
+  start /wait load-libs.bat
+  cd../..
 )
